be3006f67eb1770bc461f0ef04996deeddf6c7f4
borisecfsa <borisc@freundsa.com>
2026-07-17 16:09:26 +0000

    Eliminar la validación de actividad llena en el registro de estudiantes

--- src/app.py
+++ src/app.py
@@ -19,6 +19,7 @@ current_dir = Path(__file__).parent
 app.mount("/static", StaticFiles(directory=os.path.join(Path(__file__).parent,
           "static")), name="static")
 
+
 # In-memory activity database
 activities = {
     "Chess Club": {
@@ -65,10 +66,6 @@ def signup_for_activity(activity_name: str, email: str):
     # Validate student is not already signed up
     if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student already signed up for this activity")
-
-    # Validate activity is not full
-    if len(activity["participants"]) >= activity["max_participants"]:
-        raise HTTPException(status_code=400, detail="Activity is full")
 
     # Add student
     activity["participants"].append(email)
